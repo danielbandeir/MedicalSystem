0d8876cbee518a0b73a7eea2f3fa5ba9fd1ff269
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-04-26 23:20:25 -0300

    Updated structure folder

--- medico/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import createMedico, buscaMedico, deletarMedic, editarMedi
-
-urlpatterns = [
-    path('adicionar/', createMedico, name='criarMedico'),
-    path('buscar/', buscaMedico, name='buscarMedico'),
-    path('deletar/<int:pk>', deletarMedic, name="deletarMedico"),
-    path('editar/<int:pk>', editarMedi, name="editarMedico"),
-]
--- paciente/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import paciente, adicionarAtendimento
-
-admin.site.register(paciente)
-admin.site.register(adicionarAtendimento)
